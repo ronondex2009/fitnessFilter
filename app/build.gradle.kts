82b634334fdf349667cb919ef198b576a9f1ea74
ronondex2009 <ronandex2009@gmail.com>
2026-05-16 18:40:28 -0400

    Setup commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // Use JUnit test framework.
+    testImplementation(libs.junit)
+
+    // This dependency is used by the application.
+    implementation(libs.guava)
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass = "org.ronondex2009.App"
+}
